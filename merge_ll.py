f73bc3491a00b62e969d1bf9edc32913216bef3d
Damayanti Ghosh <damayantighosh2@gmail.com>
2017-09-28 17:17:19 -0700

    Add files via upload
    Merging two sorted linked lists

---
+++ merge_ll.py
@@ -0,0 +1,24 @@
+class ListNode:
+	def __init__(self,x):
+		self.val = x
+		self.next = None
+class Solution:
+	#@param 2 ListNode
+	#@return a ListNode
+	def mergeTwoLists(self,l1,l2):
+		dummy = ListNode(0)
+		pointer = dummy
+
+		while l1!=None and l2!=None :
+			if l1.val < l2.val :
+				pointer.next = l1
+				l1 = l1.next
+			else:
+				pointer.next = l2
+				l2 = l2.next
+			pointer = pointer.next
+		if l1 == None:
+			pointer.next = l2
+		else:
+			pointer.next = l1
+		return dummy.next
