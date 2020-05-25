6e1812e1239768bd0703631f305305a386ef50ca
Damayanti Ghosh <damayant@ualberta.ca>
2020-05-24 21:43:54 -0400

    Kadane's Algo - Highest sum of contigous integers in a subset

---
+++ src/algorithm/kadane.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+public class kadane {
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{1, 5, -1, 0, 10} ;
+        int result = highestSubarraySum(arr);
+        System.out.println(result);
+    }
+    /* this solution works for both +ve and -ve integers in array*/
+    public static int highestSubarraySum(int[] arr)
+    {
+        int max_so_far = Integer.MIN_VALUE ;
+        int max_at_present_index = arr[0];
+
+        for(int i=1;i<arr.length;i++)
+        {
+            if((max_at_present_index+arr[i])>arr[i])
+            {
+                max_at_present_index += arr[i] ;
+            }
+
+            if(max_so_far<max_at_present_index)
+            {
+                max_so_far = max_at_present_index ;
+
+            }
+        }
+        return max_so_far ;
+    }
+}
