04d824bab74ae70896aff7e030dd45c3ad0868c5
Damayanti Ghosh <damayant@ualberta.ca>
2020-05-30 19:12:00 -0400

    Two Sum problem

--- src/algorithm/arrayMain.java
+++ src/algorithm/arrayMain.java
@@ -1,4 +1,16 @@
 package algorithm;
 
 public class arrayMain {
+    public static void main(String[] args) {
+        int[] arr = {-7, -5, -3, -1, 0, 1, 3, 5, 7};
+        int targetSum = -5;
+        twoSum sum = new twoSum();
+        int[] result =sum.checkSum(arr,targetSum) ;
+        for(int i=0;i<result.length;i++)
+        {
+            System.out.println(result[i]);
+
+        }
+
+    }
 }
--- src/algorithm/twoSum.java
+++ src/algorithm/twoSum.java
@@ -1,4 +1,25 @@
 package algorithm;
 
+import java.util.HashMap;
+
 public class twoSum {
+    public int[] checkSum(int[] arr,int targetSum)
+    {
+        int[] result = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0;i<arr.length;i++)
+        {
+            int remainder = targetSum - arr[i] ;
+            if(map.containsValue(remainder))
+            {
+                result[0] = arr[i];
+                result[1] = remainder ;
+            }
+            else
+            {
+                map.put(i,arr[i]);
+            }
+        }
+        return result;
+    }
 }
