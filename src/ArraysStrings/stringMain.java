57b31fbf2d2c1062ed0e8f9e0245ea9bef7b82b8
Damayanti Ghosh <damayant@ualberta.ca>
2020-05-31 23:09:44 -0400

    Longest Substring with unique charecters

---
+++ src/ArraysStrings/stringMain.java
@@ -0,0 +1,11 @@
+package ArraysStrings;
+
+public class stringMain {
+    public static void main(String[] args) {
+        //Longest Unique Substring
+        String str = "pwwkew";
+        longestUniqueSubstring len = new longestUniqueSubstring();
+        System.out.println(len.lengthOfLongestSubstring(str));
+
+    }
+}
