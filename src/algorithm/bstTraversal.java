ec7d31280b8d4e995d7eb17b499fad41fa67b85f
Damayanti Ghosh <damayant@ualberta.ca>
2020-12-18 07:46:18 -0500

    Update bstTraversal.java
    added zigzag / spiral level order traversal

--- src/algorithm/bstTraversal.java
+++ src/algorithm/bstTraversal.java
@@ -37,5 +37,46 @@ public class bstTraversal {
         postorder(node.right);
         System.out.print(node.key+ " ");
     }
+    //zigzag / spiral level order traversal
+    public void spiralWithTwoStack(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        Stack<Node> s1=new Stack<>();
+        Stack<Node> s2=new Stack<>();
+        s1.push(root);
+
+        while(!s1.isEmpty()||!s2.isEmpty())
+        {
+            while (!s1.isEmpty())
+            {
+                root = s1.pop();
+                System.out.print(root.key+" ");
+                if(root.left!=null)
+                {
+                    s2.push(root.left);
+                }
+                if(root.right!=null)
+                {
+                    s2.push(root.right);
+                }
+            }
+            while(!s2.isEmpty())
+            {
+                root=s2.pop();
+                System.out.print(root.key+" ");
+                if(root.right!=null)
+                {
+                    s1.push(root.right);
+                }
+                if(root.left!=null)
+                {
+                    s2.push(root.left);
+                }
+            }
+        }
+    }
 
 }
